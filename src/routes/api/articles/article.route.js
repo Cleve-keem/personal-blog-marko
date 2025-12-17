e887b2d500a9256096cb3dae77a9c83dfa2e7cb3
Cleve-keem <belloharyourmidey@gmail.com>
2025-12-17 03:09:35 -0600

    feat(chore): restyled the dashboard artice list

--- src/routes/api/articles/article.route.js
+++ src/routes/api/articles/article.route.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const ArticleController = require("../../../controllers/article.controller.js");
 
 router.post("/article/new", ArticleController.addNewArticle);
+router.post("/article/edit/:id", ArticleController.addNewArticle);
 
 module.exports = router;
